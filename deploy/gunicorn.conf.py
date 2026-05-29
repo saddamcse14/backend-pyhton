ea6af92c5723652bc9ac16c5b6a332722f114bc5
iosdev <iosdev@iosdevs-MacBook-Air.local>
2026-05-29 10:20:10 +0600

    set users one

--- deploy/gunicorn.conf.py
+++ deploy/gunicorn.conf.py
@@ -1,9 +1,8 @@
-import multiprocessing
 import os
 
 port = int(os.environ.get('PORT', 8000))
 bind = f'0.0.0.0:{port}'
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 worker_class = 'sync'
 accesslog = '-'
 errorlog = '-'
